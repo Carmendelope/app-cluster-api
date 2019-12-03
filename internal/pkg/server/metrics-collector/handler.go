e286fc01d57387f14708d52e97a9e0acebe7306a
Marcos Garcia <thiamath@gmail.com>
2019-12-03 15:29:49 +0100

    NP-2379 Public GetContainerStats request.

--- internal/pkg/server/metrics-collector/handler.go
+++ internal/pkg/server/metrics-collector/handler.go
@@ -12,17 +12,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package metrics_collector
 
 import (
 	"context"
-	"github.com/nalej/derrors"
 	"github.com/nalej/grpc-common-go"
-	grpc "github.com/nalej/grpc-monitoring-go"
-	"github.com/nalej/grpc-utils/pkg/conversions"
+	"github.com/nalej/grpc-monitoring-go"
 	"github.com/rs/zerolog/log"
 )
 
@@ -36,23 +33,24 @@ func NewHandler(manager Manager) *Handler {
 }
 
 // Retrieve a summary of high level cluster resource availability
-func (h *Handler) GetClusterSummary(ctx context.Context, request *grpc.ClusterSummaryRequest) (*grpc.ClusterSummary, error) {
+func (h *Handler) GetClusterSummary(ctx context.Context, request *grpc_monitoring_go.ClusterSummaryRequest) (*grpc_monitoring_go.ClusterSummary, error) {
 	log.Debug().Interface("request", request).Msg("GetClusterSummary")
 	return h.Manager.GetClusterSummary(request)
 }
 
 // Retrieve statistics on cluster with respect to platform resources
-func (h *Handler) GetClusterStats(ctx context.Context, request *grpc.ClusterStatsRequest) (*grpc.ClusterStats, error) {
+func (h *Handler) GetClusterStats(ctx context.Context, request *grpc_monitoring_go.ClusterStatsRequest) (*grpc_monitoring_go.ClusterStats, error) {
 	log.Debug().Interface("request", request).Msg("GetClusterStats")
 	return h.Manager.GetClusterStats(request)
 }
 
 // Execute a query directly on the monitoring storage backend
-func (h *Handler) Query(ctx context.Context, request *grpc.QueryRequest) (*grpc.QueryResponse, error) {
+func (h *Handler) Query(ctx context.Context, request *grpc_monitoring_go.QueryRequest) (*grpc_monitoring_go.QueryResponse, error) {
 	log.Debug().Interface("request", request).Msg("Query")
 	return h.Manager.Query(request)
 }
 
-func (h *Handler) GetContainerStats(ctx context.Context, in *grpc_common_go.Empty) (*grpc.ContainerStatsResponse, error) {
-	return nil, conversions.ToGRPCError(derrors.NewUnimplementedError("not implemented yet"))
+func (h *Handler) GetContainerStats(ctx context.Context, request *grpc_common_go.Empty) (*grpc_monitoring_go.ContainerStatsResponse, error) {
+	log.Debug().Msg("GetContainerStats")
+	return h.Manager.GetContainerStats(request)
 }
